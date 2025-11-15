89e634f67cf00ce8597b458e9619e0bf05a088a1
DinoDotRawr <dushravifuture@gmail.com>
2025-11-15 00:10:45 -0500

    Commands in Update Periodic and Subsystems

--- src/main/java/frc/robot/commands/Auton.java
+++ src/main/java/frc/robot/commands/Auton.java
@@ -1,9 +1,10 @@
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Robot;
 import frc.robot.constants.RobotConstants;
 
-public class Auton {
+public class Auton extends SubsystemBase {
 
     /**
      * 
--- src/main/java/frc/robot/commands/Manipulator.java
+++ src/main/java/frc/robot/commands/Manipulator.java
@@ -1,6 +1,8 @@
 package frc.robot.commands;
 
-public class Manipulator {
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Manipulator extends SubsystemBase {
 
     /**
      * 
